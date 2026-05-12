f4ee9b0de37415a1dba64c2e00f7aad1d1e7adbd
xushnudbek-prog <xushnudbek756@gmail.com>
2026-05-12 18:31:33 +0500

    Initial commit

---
+++ resource-app/src/main/java/uz/uptimehub/resourceapp/ResourceAppApplication.java
@@ -0,0 +1,13 @@
+package uz.uptimehub.resourceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceAppApplication.class, args);
+    }
+
+}
