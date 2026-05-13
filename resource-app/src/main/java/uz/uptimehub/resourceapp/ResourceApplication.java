7dd50d2731a5b71d0a78f268b3068bd54ce5854f
xushnudbek-prog <xushnudbek756@gmail.com>
2026-05-13 12:07:58 +0500

    Rename application and test classes for consistency

--- resource-app/src/main/java/uz/uptimehub/resourceapp/ResourceApplication.java
+++ resource-app/src/main/java/uz/uptimehub/resourceapp/ResourceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResourceAppApplication {
+public class ResourceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ResourceAppApplication.class, args);
+    static void main(String[] args) {
+        SpringApplication.run(ResourceApplication.class, args);
     }
 
 }
