1bd9d50b0ca89830011e9c97f68e56fa14621c22
xushnudbek-prog <xushnudbek756@gmail.com>
2026-05-13 16:39:05 +0500

    Add Category entity and Status enum definitions

---
+++ resource-app/src/main/java/uz/uptimehub/resourceapp/jpa/entity/Status.java
@@ -0,0 +1,5 @@
+package uz.uptimehub.resourceapp.jpa.entity;
+
+public enum Status {
+    PUBLISHED, ARCHIVED, NOT_PUBLISHED
+}
---
+++ resource-app/src/main/java/uz/uptimehub/resourceapp/jpa/entity/category/Category.java
@@ -0,0 +1,35 @@
+package uz.uptimehub.resourceapp.jpa.entity.category;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import uz.uptimehub.resourceapp.jpa.entity.Status;
+import uz.uptimehub.resourceapp.jpa.entity.UserAuditableEntity;
+
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Category extends UserAuditableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+}
