66c442c8ab6fa057bd764440d68d95bd28e5a15d
xushnudbek-prog <xushnudbek756@gmail.com>
2026-05-13 16:51:20 +0500

    Add auditing listener support in DateAuditableEntity

--- resource-app/src/main/java/uz/uptimehub/resourceapp/jpa/entity/DateAuditableEntity.java
+++ resource-app/src/main/java/uz/uptimehub/resourceapp/jpa/entity/DateAuditableEntity.java
@@ -1,17 +1,20 @@
 package uz.uptimehub.resourceapp.jpa.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class DateAuditableEntity {
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
