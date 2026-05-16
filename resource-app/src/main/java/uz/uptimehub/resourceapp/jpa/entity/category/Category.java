40a413190b1ac4a4ae3543529656316d9285d725
xushnudbek-prog <xushnudbek756@gmail.com>
2026-05-16 11:54:43 +0500

    Remove unused auditing listener import from Category entity

--- resource-app/src/main/java/uz/uptimehub/resourceapp/jpa/entity/category/Category.java
+++ resource-app/src/main/java/uz/uptimehub/resourceapp/jpa/entity/category/Category.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import uz.uptimehub.resourceapp.jpa.entity.Status;
 import uz.uptimehub.resourceapp.jpa.entity.UserAuditableEntity;
 
