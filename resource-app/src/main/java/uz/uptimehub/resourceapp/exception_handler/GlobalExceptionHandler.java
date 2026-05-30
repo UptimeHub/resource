0859cdb68cf14fcdb322b6a45bb78820d6851bde
xushnudbek-prog <xushnudbek756@gmail.com>
2026-05-30 12:44:07 +0500

    GlobalExceptionHandler implemented and version updated

---
+++ resource-app/src/main/java/uz/uptimehub/resourceapp/exception_handler/GlobalExceptionHandler.java
@@ -0,0 +1,174 @@
+package uz.uptimehub.resourceapp.exception_handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import uz.uptimehub.core.dto.ErrorResponse;
+import uz.uptimehub.core.exception.BadRequestException;
+import uz.uptimehub.core.exception.EntityAlreadyExistsException;
+import uz.uptimehub.core.exception.EntityNotFoundException;
+import uz.uptimehub.core.exception.InvalidSortRule;
+import uz.uptimehub.resource.dto.exception.RequiredSpecificationNotAvailableException;
+
+import java.nio.file.AccessDeniedException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex, HttpServletRequest request) {
+        return buildResponse(
+                HttpStatus.NOT_FOUND,
+                "Entity Not Found",
+                ex.getMessage(),
+                request.getRequestURI(),
+                null
+        );
+    }
+
+    @ExceptionHandler(InvalidSortRule.class)
+    public ResponseEntity<ErrorResponse> handleInvalidSortRule(InvalidSortRule invalidSortRule, HttpServletRequest request) {
+        return buildResponse(
+                HttpStatus.BAD_REQUEST,
+                "Invalid Sort Rule",
+                invalidSortRule.getMessage(),
+                request.getRequestURI(),
+                null
+        );
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex, HttpServletRequest request) {
+        return buildResponse(
+                HttpStatus.BAD_REQUEST,
+                "Bad Request",
+                ex.getMessage(),
+                request.getRequestURI(),
+                null
+        );
+    }
+
+    @ExceptionHandler(EntityAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEntityAlreadyExistsException(EntityAlreadyExistsException ex, HttpServletRequest request) {
+        return buildResponse(
+                HttpStatus.CONFLICT,
+                "Entity Already Exists",
+                ex.getMessage(),
+                request.getRequestURI(),
+                null
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request
+    ) {
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                fieldErrors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return buildResponse(
+                HttpStatus.BAD_REQUEST,
+                "VALIDATION_ERROR",
+                "Validation failed",
+                request.getRequestURI(),
+                fieldErrors
+        );
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolation(
+            ConstraintViolationException ex,
+            HttpServletRequest request
+    ) {
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        ex.getConstraintViolations().forEach(violation ->
+                fieldErrors.put(
+                        violation.getPropertyPath().toString(),
+                        violation.getMessage()
+                )
+        );
+
+        return buildResponse(
+                HttpStatus.BAD_REQUEST,
+                "VALIDATION_ERROR",
+                "Validation failed",
+                request.getRequestURI(),
+                fieldErrors
+        );
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDenied(
+            HttpServletRequest request
+    ) {
+        return buildResponse(
+                HttpStatus.FORBIDDEN,
+                "FORBIDDEN",
+                "Access denied",
+                request.getRequestURI(),
+                null
+        );
+    }
+
+    @ExceptionHandler(RequiredSpecificationNotAvailableException.class)
+    public ResponseEntity<ErrorResponse> handleRequiredSpecificationNotAvailableException(RequiredSpecificationNotAvailableException ex) {
+        return buildResponse(
+                HttpStatus.BAD_REQUEST,
+                "Required Specifications are not provided",
+                ex.getMessage(),
+                null,
+                null
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpected(
+            Exception ex,
+            HttpServletRequest request
+    ) {
+        log.error("Unexpected error occurred", ex);
+
+        return buildResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "INTERNAL_SERVER_ERROR",
+                "Unexpected internal server error",
+                request.getRequestURI(),
+                null
+        );
+    }
+
+
+
+    private ResponseEntity<ErrorResponse> buildResponse(
+            HttpStatus status,
+            String error,
+            String message,
+            String path,
+            Map<String, String> fieldErrors
+    ) {
+        ErrorResponse response = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .error(error)
+                .message(message)
+                .path(path)
+                .fieldErrors(fieldErrors)
+                .build();
+
+        return ResponseEntity.status(status).body(response);
+    }
+}
